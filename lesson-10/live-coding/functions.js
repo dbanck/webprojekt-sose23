cbd8cfb018615234ea098205ecdcbc911b5b8746
Basti Buck <mail@bastibuck.de>
2023-06-19 19:52:20 +0200

    live coding lesson 10

--- lesson-10/live-coding/functions.js
+++ lesson-10/live-coding/functions.js
@@ -1,1 +1,20 @@
-console.log("JS loaded");
+let likeCount = 0;
+
+function like() {
+  const likeElement = document.getElementById("likeCount");
+
+  // count up
+  likeCount = likeCount + 1;
+
+  likeElement.textContent = likeCount;
+}
+
+// Alternative Lösung ohne Variable im JS
+function likeTwo() {
+  const likeElement = document.getElementById("likeCount");
+
+  const likeCountAsNumber = parseInt(likeElement.textContent);
+  const newLikeCount = likeCountAsNumber + 1;
+
+  likeElement.textContent = newLikeCount;
+}
